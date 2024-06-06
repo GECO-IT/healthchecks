10c44bedbceeeee03b45a93ef530e1625409c1e1
Tgrl84250 <t.guy.lemier@gmail.com>
2024-06-06 15:43:32 +0200

    Revert "Add True Read-Only Role Preventing Project Creation"
    This reverts commit 29165f3b7b66eb40760de491c52e261ee09de5b6.

--- static/js/projects_menu.js
+++ static/js/projects_menu.js
@@ -14,7 +14,6 @@ $(function() {
             timeout: 2000,
             success: function(data) {
                 $("#project-menu li.project-item").remove();
-                $("#project-menu .new_project").remove();
                 $("#projects-divider").after(data);
             }
         });
